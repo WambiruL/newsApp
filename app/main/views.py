08dfaf3dc961e134d7ddf2acdce0edc6674af018
WambiruL <wambirulorraine@gmail.com>
2021-06-13 00:55:10 +0300

    Create a search view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from ..requests import get_sources,get_articles, articles_source
+from ..requests import get_sources,get_articles, articles_source,search_articles
 from . import main
 
 
@@ -33,3 +33,16 @@ def sourceArticles(id):
     print(all_articles)
     source = id
     return render_template('sourcearticles.html', articles = all_articles, source = source)
+
+@main.route('/search/<article_name>')
+def articleSearch(article_name):
+    """
+    function that returns the searched articles
+    """
+    search_article_name = article_name.split("")
+    search_name_format = "+".join(search_article_name)
+    searched_articles = search_articles(search_name_format)
+
+    return render_template('search.html',articles = searched_articles)
+
+
